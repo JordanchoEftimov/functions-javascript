6e8221389f81a48b8395e6377c73de9565bb53ed
Jordancbo Eftimov <ejordancho@gmail.com>
2023-02-25 18:27:34 +0100

    task 5

---
+++ task5.js
@@ -0,0 +1,10 @@
+function checkOddEvenNumber(number) {
+    if (number % 2 === 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+checkOddEvenNumber(10);
+checkOddEvenNumber(11);
